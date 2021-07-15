4ea8c24a8298d15594ab43769f862241fc1dce3e
Your Name <you@example.com>
2021-07-15 15:51:12 +0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AuthInterceptor } from "./auth/auth-interceptor";
+import { ErrorInterceptor } from "./error-interceptor";
+import { ErrorComponent } from "./error/error.component";
+import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from "./posts/posts.module";
+import { FooterComponent } from "./footer/footer.component";
+import { AuthModule } from "./auth/auth.module";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ErrorComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    PostsModule,
+    AuthModule,
+    CKEditorModule,
+    FormsModule,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent],
+})
+export class AppModule {}
